15bd7860b404fc1ff0c239ddfd76febc46c1ed51
김지은 <jxixeun@gmail.com>
2023-03-30 11:14:29 +0900

    :rocket: [Feat] PlaceDto 생성

---
+++ src/com/tripgogo/mytrip/model/PlaceDto.java
@@ -0,0 +1,37 @@
+package com.tripgogo.mytrip.model;
+
+public class PlaceDto {
+	
+	private String placeName;
+	private String category;
+	private String x;
+	private String y;
+	
+	public String getPlaceName() {
+		return placeName;
+	}
+	
+	public void setPlaceName(String placeName) {
+		this.placeName = placeName;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getX() {
+		return x;
+	}
+	public void setX(String x) {
+		this.x = x;
+	}
+	public String getY() {
+		return y;
+	}
+	public void setY(String y) {
+		this.y = y;
+	}
+	
+	
+}
