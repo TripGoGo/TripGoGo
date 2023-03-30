00637bf3d1a0925444471883fe00056a2dec60ab
김지은 <jxixeun@gmail.com>
2023-03-30 11:48:46 +0900

    :rocket: [Feat] MyTripDto 생성

--- src/com/tripgogo/mytrip/model/MytripDto.java
+++ src/com/tripgogo/mytrip/model/MytripDto.java
@@ -1,5 +1,16 @@
 package com.tripgogo.mytrip.model;
 
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MytripDto {
+	private String city;
+	private Date startDate;
+	private Date endDate;
+	private int period;
+	private String companion;
+	private String tripStyle;
+//	private List<List<PlaceDto>> plans;
 	
 }
